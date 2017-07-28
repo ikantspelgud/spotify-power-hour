b2bcbfcc09c84477697e62635e6c4549a5f66d38
Tom <ikantspelgud@gmail.com>
2017-07-28 12:19:26 -0500

    Basic power hour functionality! Moved spotify-web-api into our project because there was an error with next and prev that I fixed. Also added some stuff for doing fading.

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 
 const CLIENT_ID = "16d85cb583694305a0e11b713a97ef21";
 const REDIRECT_URI = encodeURIComponent("http://localhost:3000/player");
-const AUTH_URL = `http://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-read-private%20playlist-read-collaborative%20user-library-read&state=123&redirect_uri=${REDIRECT_URI}`;
+
+//const AUTH_URL = `http://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-read-private%20playlist-read-collaborative%20user-library-read&state=123&redirect_uri=${REDIRECT_URI}`;
+const AUTH_URL = `http://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-read-private%20playlist-read-collaborative%20user-library-read%20user-modify-playback-state&state=123&redirect_uri=${REDIRECT_URI}`;
 
 export default class LoginButton extends Component {
   authorize() {
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,69 +1,131 @@
 import React, { Component } from 'react';
-import SpotifyWebApi from 'spotify-web-api-js';
-import Iframe from 'react-iframe';
+import SpotifyWebApi from '../spotify-web-api-js';
 import queryString from 'query-string';
+import ReactInterval from 'react-interval';
+import {Button, Collapse, Well, ButtonToolbar} from 'react-bootstrap';
+
 
 export default (props) => {
   const parsedHash = queryString.parse(props.location.hash);
   return (
-    <div>
+    <div className="playerBtn">
       <div>
         <Player authToken={parsedHash.access_token} />
       </div>
-      <div>
-        <PlayerWindow/>
-      </div>
     </div>
   );
-}
-
-
-
-class PlayerWindow extends Component {
-
-  render(){
-    return (
-      <Iframe url="https://open.spotify.com/embed?uri=spotify:track:3zSCNTXI7Ed0PiidZVmzIe" width="90%" height="90%" frameborder="0" allowtransparency="true"></Iframe>
-      )
-  }
 }
 
 class Player extends Component {
 
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.onPlay = this.onPlay.bind(this);
+    this.onNext = this.onNext.bind(this);
+    this._doFade = this._doFade.bind(this);
+    this.onPowerHour = this.onPowerHour.bind(this);
+    this.state = {
+      powerHourEnabled: false, 
+      spotifyApi: new SpotifyWebApi(), 
+      timeoutLength: 60000,
+      fadeoutLength: 3000,
+      fadeinLength: 2000,
+      curVolume: null,
+      goalVolume: null,
+      isFading: false,
+      fadeDirection: 1,
+      fadeInterval: 500,
+      fadeAmount: 10
+    }
+    this.state.spotifyApi.setAccessToken(this.props.authToken)
   }
 
-  handleClick() {
+  fadeVolume(props) {
+    this.setState({
+      curVolume: props.curVolume, //probably wanna get this from spotify eventually
+      goalVolume: props.goalVolume,
+      fadeAmount: Math.round(Math.abs(props.curVolume - props.goalVolume) / ((props.time/1000) / .25)), 
+      fadeInterval: 5,//props.time / Math.abs(props.curVolume - props.goalVolume),
+      fadeDirection: props.curVolume > props.goalVolume ? -1 : 1,
+      isFading: true
+    })
+  }
+
+  _doFade() {
+    this.setState({curVolume: this.state.fadeDirection * this.state.fadeAmount + this.state.curVolume})
     
+    if(this.state.fadeDirection === 1){
+      if(this.state.curVolume >= this.state.goalVolume){
+        this.setState({isFading: false})
+      }
+    }
+    else if (this.state.curVolume <= this.state.goalVolume){
+        this.setState({isFading: false})
+    }
+    else{
+      this.state.spotifyApi.setVolume(this.state.curVolume, {})
+    }
+  }
 
-    //get this by doing a request on the spotify web api console and looking what it uses for bearer
-    var authToken = this.props.authToken || 'BQCz-IHnREjJiTIxuIkvMaDHFTM_GZrzbLORiXGEiwYKHGYm72OhDhaQOxRGg232Lqgq9M4A-c0leiEzkqfCIqkBHjuAPgrqdkANlbCyrRHq5mQpcWbP9UzWZrTO5v0htLK3ZYHeN8STc6pSHxm1KDXXyQzCKTvszw3tRX_FdsI';
+  onPlay() {
+    this.state.spotifyApi.play({"context_uri": "spotify:user:1247199566:playlist:3lA4R6BviqvdRcFnp9gMlH", "offset": {"position": 5}})
+  }
 
-    var spotifyApi = new SpotifyWebApi();
-    spotifyApi.setAccessToken(authToken);
-    var info = spotifyApi.getMyDevices()  // note that we don't pass a user id
-      .then(function(data) {
-        //console.log('device' , data.devices[0].id);
-        console.log('User playlists', data);
-        spotifyApi.play({"context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr", "offset": {"position": 5}})
+  onPowerHour() {
+    this.setState({powerHourEnabled: !this.state.powerHourEnabled})
 
-      }, function(err) {
-        console.error(err);
-      });
+  }
 
-    console.log(info)
+  onNext() {
+    this.fadeVolume({curVolume:100, goalVolume:0, time:3000})
+    this.state.spotifyApi.skipToNext({})
+    this.fadeVolume({curVolume:0, goalVolume:100, time:3000})
+    console.log("timer trigger")
   }
 
 
 
   render() {
+    const divCenter = {
+      marginLeft: "auto", 
+      marginRight: "auto"
+    }
+
     return (
       <div>
-        <button type="button" onClick={this.handleClick}>
-        Play
-        </button>
+
+        <Button bsSize="large" onClick={ ()=> this.setState({ open: !this.state.open })}>
+          Instructions
+        </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Well>
+              Instructions: Spotify must be open. The power hour button will make 
+              whatever you are playing a power hour. If you don't know what to play
+              the play button will start the dopest of playlists.
+            </Well>
+          </div>
+        </Collapse>
+
+        <ReactInterval 
+          time={this.state.fadeInterval} 
+          enabled={this.state.isFading} 
+          callback={this._doFade}/>
+        
+        <ReactInterval 
+          timeout={this.state.timeoutLength} 
+          enabled={this.state.powerHourEnabled}
+          callback={this.onNext}/>
+        <div style={divCenter}>
+          <ButtonToolbar>  
+            <Button bsSize="large" bsStyle="primary" onClick={this.onPlay}>
+            Play
+            </Button>
+            <Button bsSize="large" bsStyle="primary" onClick={this.onPowerHour}>
+            Power Hour!!!
+            </Button>
+          </ButtonToolbar>
+        </div>
       </div>
     );
   }
