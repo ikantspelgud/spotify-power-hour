50a84b17f12f23245637239d54f5e0497b76b88f
Kathleen Garitty <katgaritty@gmail.com>
2017-08-02 10:05:10 -0500

    update router path options for github pages

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ class App extends Component {
         <div className="App-intro">
           <Router>
               <Switch>
+                <Route path="/spotify-power-hour/player" component={Player}/>
                 <Route path="/player" component={Player}/>
+                <Route path="/spotify-power-hour" component={LoginButton}/>
                 <Route path="/" component={LoginButton}/>
               </Switch>
           </Router>
